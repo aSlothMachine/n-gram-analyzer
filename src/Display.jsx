4709b43483aef52384fba429b512f401a9b7437a
Richie <ric.inbox.edu@gmail.com>
2023-09-06 15:37:44 -0700

    Update

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Display = ({ comparisons }) => (
-    <div className='display'>
-        <h2>Comparisons</h2>
-        <ul>
-            {comparisons.map((comparison, index) => (
-                <li key={index}>{comparison}</li>
-            ))}
-        </ul>
-    </div>
+  <div className='display'>
+    <h2>Jaccard Similarity Comparisons:</h2>
+    <ul>
+      {comparisons.map((comparison, index) => (
+        <li key={index}>
+          {`Jac(${comparison.file1}, ${comparison.file2}) = ${comparison.similarity}`}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Display;
