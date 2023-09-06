621b17dcadb76c4efd01aa5cd272dd386726b67a
Richie <ric.inbox.edu@gmail.com>
2023-09-06 13:01:21 -0700

    API testing

--- src/Submit.jsx
+++ src/Submit.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import Display from './Display';
 
-const Submit = ({ file }) => {
-    const [selectedFiles, setSelectedFiles] = useState([]); // default to empty ra.
+const Submit = () => {
+    const [selectedFiles, setSelectedFiles] = useState([]);
     const [comparisons, setComparisons] = useState([]);
     const [showComparisons, setShowComparisons] = useState(false);
-    const [selectedGram, setSelectedGram] = useState(1);    // default to one.
+    const [selectedGram, setSelectedGram] = useState(1);
 
     const handleFile = (e) => {
         const files = Array.from(e.target.files);
@@ -13,28 +13,48 @@ const Submit = ({ file }) => {
         if (files.length >= 2 && files.length <= 3) {
             setSelectedFiles(files);
         } else {
-            alert('Please select 2 to 3 files (inclusive');
+            alert('Please select 2 to 3 files (inclusive)');
         }
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (selectedFiles.length < 2) {
             alert('Please select at least 2 text files.');
             return;
         }
 
-        // begin calculating similarity.
-        // performComparisons();
+        // Create a FormData object and append selected files
+        const formData = new FormData();
+        selectedFiles.forEach((file, index) => {
+            formData.append(`files`, file); // ?
+        });
 
-        // update the page on submit.
-        setShowComparisons(true);
+        try {
+            const response = await fetch('http://localhost:3001/api/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                // Handle success, e.g., update state with comparisons
+                const data = await response.json();
+                setComparisons(data.comparisons);
+                setShowComparisons(true);
+            } else {
+                // Handle server error, e.g., show an error message
+                alert('Server error. Please try again later.');
+            }
+        } catch (error) {
+            // Handle fetch error, e.g., show an error message
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        }
     }
 
-    // calculate here ?
     return (
         <div className="submit">
-            <h2> File Upload & n-gram selection:</h2>
-            <input type="file" multiple onChange={handleFile} />
+            <h2>File Upload & n-gram selection:</h2>
+            <input type="file" name="files" multiple onChange={handleFile} />
             <br />
             <label>
                 Select n-gram value:
